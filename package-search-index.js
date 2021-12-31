fc6300bef2e2daf00bcee512c931879a8c216207
alexiavp <alexia.vidal.pons@gmail.com>
2021-12-31 12:58:37 +0100

    Final

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"composite"},{"l":"dataFrame"},{"l":"factory"},{"l":"mapReduce"},{"l":"observer"},{"l":"Test"},{"l":"visitor"}];updateSearchResults();
